0edf3531801e7db92ca3ed0cdb1b1fff7a1322bb
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-09-27 07:43:53 +0800

    feat: changes

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,14 +2,11 @@ import React from "react";
 
 import BenefitsSection from "@/components/home/sections/Benefits";
 import BestSellersSection from "@/components/home/sections/BestSellers";
-import FAQsSection from "@/components/home/sections/FAQs";
 import HeaderSection from "@/components/home/sections/Header";
 import NewArrivals from "@/components/home/sections/NewArrival";
 import NewArrivals2 from "@/components/home/sections/NewArrival2";
 import PopluarCategoriesSection from "@/components/home/sections/PopluarCategories";
 import RecommendedSection from "@/components/home/sections/Recommend";
-
-import StylesSection from "@/components/home/sections/StylesSection";
 
 const page = () => {
   return (
@@ -17,7 +14,6 @@ const page = () => {
       <HeaderSection />
       <BestSellersSection />
 
-      <StylesSection />
       <BenefitsSection />
       <NewArrivals2 />
       <RecommendedSection />
--- src/shared/Footer/Footer.tsx
+++ src/shared/Footer/Footer.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import FooterBottom from './FooterBottom';
-import FooterMain from './FooterMain';
+import FooterMain from "./FooterMain";
 
 const Footer: React.FC = () => {
   return (
     <footer className="bg-white dark:bg-neutral-900">
       <div className="container pt-16">
         <FooterMain />
-        <FooterBottom />
       </div>
     </footer>
   );
